7f1f2e241929c05fdb251122444e4116277d6fd2
seocontent1 <seocontentmachine11@gmail.com>
2025-11-18 14:29:23 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/quizbatler/", // adicione isso
   plugins: [
     react(),
     runtimeErrorOverlay(),
