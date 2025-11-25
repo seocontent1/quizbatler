c6a1b64ed1e6bf7ac8cfd3b2ee35b096b51a9178
Markes Salles <108896525+openbakk@users.noreply.github.com>
2025-11-25 12:49:30 -0300

    adadaadada

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { initIAP } from "./services/iap";
 
 createRoot(document.getElementById("root")!).render(<App />);
 
